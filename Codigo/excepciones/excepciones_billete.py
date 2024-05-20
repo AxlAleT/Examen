d6c2250f0f4efb4a4faeda648462f424e973f7fb
Omar AC <109171712+OmarAC03@users.noreply.github.com>
2024-05-20 09:33:31 -0600

    excepciones_billete.py

--- Codigo/excepciones/excepciones_billete.py
+++ Codigo/excepciones/excepciones_billete.py
@@ -1,15 +1,15 @@
 class NoSePuedeDarMontoException(Exception):
     pass
- """
+    """
     Excepción lanzada cuando no es posible entregar un monto solicitado con los billetes disponibles.
     """
 class NoHaySolucionEficienteException(Exception):
     pass
- """
+    """
     Excepción lanzada cuando no hay una solución eficiente para entregar el monto solicitado con los billetes disponibles.
     """
 class DenominacionNoExistente(Exception):
     pass
-       """
+    """
     Excepción lanzada cuando se intenta agregar un billete de una denominación que no existe en el sistema.
     """
