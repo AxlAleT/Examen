3dd63f562525162190cea8ee338017efe8e85024
Omar AC <109171712+OmarAC03@users.noreply.github.com>
2024-05-20 10:59:32 -0600

    seleccionarServicio.py

--- Codigo/vistas/seleccionarServicio.py
+++ Codigo/vistas/seleccionarServicio.py
@@ -2,12 +2,32 @@ import wx
 from controladores.Servicio_controlador import ServicioControlador
 
 class SeleccionarServicio(wx.Frame):
+    """
+    Clase que representa la ventana para seleccionar un servicio para el pago sin tarjeta.
+
+    Attributes:
+        parent (wx.Window): La ventana principal o padre de esta ventana.
+
+    Methods:
+        __init__(parent): Inicializa la ventana de selección de servicio.
+        InitUI(): Inicializa la interfaz de usuario de la ventana.
+        on_item_selected(event): Maneja el evento de selección de un servicio.
+        cerrar_frame(event): Cierra la ventana actual.
+    """
+
     def __init__(self, parent):
+        """
+        Inicializa la ventana de selección de servicio.
+
+        Args:
+            parent (wx.Window): La ventana principal o padre de esta ventana.
+        """
         super().__init__(parent, title='Seleccionar Servicio', size=(400, 300))
         self.parent = parent  # Guardar el parent para usarlo más tarde
         self.InitUI()
 
     def InitUI(self):
+        """Inicializa la interfaz de usuario de la ventana."""
         panel = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
 
@@ -39,6 +59,12 @@ class SeleccionarServicio(wx.Frame):
         self.Show()
 
     def on_item_selected(self, event):
+        """
+        Maneja el evento de selección de un servicio.
+
+        Args:
+            event: El evento que activa la función (en este caso, la selección de un elemento de la lista).
+        """
         item = event.GetItem()
         nombre = item.GetText()
         convenio = self.lista_servicios.GetItem(item.GetId(), 1).GetText()
@@ -52,19 +78,45 @@ class SeleccionarServicio(wx.Frame):
         pagoServicioSinTarjeta.Show()
 
     def cerrar_frame(self, event):
+        """
+        Cierra la ventana actual.
+
+        Args:
+            event: El evento que activa la función (en este caso, el clic en el botón).
+        """
         self.Close()
 
 
-# seleccionar_servicio_para_tarjeta.py
-
 class SeleccionarServicioParaTarjeta(wx.Frame):
+    """
+    Clase que representa la ventana para seleccionar un servicio para el pago con tarjeta.
+
+    Attributes:
+        parent (wx.Window): La ventana principal o padre de esta ventana.
+        num_tarjeta (str): El número de tarjeta asociado al usuario.
+
+    Methods:
+        __init__(parent, num_tarjeta): Inicializa la ventana de selección de servicio para tarjeta.
+        InitUI(): Inicializa la interfaz de usuario de la ventana.
+        on_item_selected(event): Maneja el evento de selección de un servicio.
+        cerrar_frame(event): Cierra la ventana actual.
+    """
+
     def __init__(self, parent, num_tarjeta):
+        """
+        Inicializa la ventana de selección de servicio para tarjeta.
+
+        Args:
+            parent (wx.Window): La ventana principal o padre de esta ventana.
+            num_tarjeta (str): El número de tarjeta asociado al usuario.
+        """
         super().__init__(parent, title='Seleccionar Servicio', size=(400, 300))
         self.parent = parent
         self.num_tarjeta = num_tarjeta
         self.InitUI()
 
     def InitUI(self):
+        """Inicializa la interfaz de usuario de la ventana."""
         panel = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
 
@@ -96,6 +148,12 @@ class SeleccionarServicioParaTarjeta(wx.Frame):
         self.Show()
 
     def on_item_selected(self, event):
+        """
+        Maneja el evento de selección de un servicio.
+
+        Args:
+            event: El evento que activa la función (en este caso, la selección de un elemento de la lista).
+        """
         item = self.lista_servicios.GetFirstSelected()
         if item == -1:
             wx.MessageBox('Por favor, seleccione un servicio', 'Info', wx.OK | wx.ICON_INFORMATION)
@@ -113,4 +171,10 @@ class SeleccionarServicioParaTarjeta(wx.Frame):
         pagoServicioConTarjeta.Show()
 
     def cerrar_frame(self, event):
+        """
+        Cierra la ventana actual.
+
+        Args:
+            event: El evento que activa la función (en este caso, el clic en el botón).
+        """
         self.Close()
