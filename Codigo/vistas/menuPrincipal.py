4a0781d40076ae2a2449512b280b6bfe8821f7e0
Omar AC <109171712+OmarAC03@users.noreply.github.com>
2024-05-20 10:41:49 -0600

    menuPrincipal.py

--- Codigo/vistas/menuPrincipal.py
+++ Codigo/vistas/menuPrincipal.py
@@ -2,7 +2,26 @@ import wx
 from vistas.insertarTarjeta import InsertarTarjeta
 
 class MenuPrincipal(wx.Frame):
+    """
+    Clase que representa la ventana del menú principal de un cajero automático.
+
+    Attributes:
+        parent (wx.Window): La ventana padre de esta ventana.
+    
+    Methods:
+        __init__(parent=None): Inicializa la ventana del menú principal.
+        depositar_efectivo(event): Abre la ventana para realizar un depósito de efectivo.
+        pagar_servicios(event): Abre la ventana para realizar el pago de servicios.
+        abrir_insertar_tarjeta(event): Abre la ventana para insertar una tarjeta en el cajero.
+    """
+
     def __init__(self, parent=None):
+        """
+        Inicializa la ventana del menú principal.
+
+        Args:
+            parent (wx.Window): La ventana padre de esta ventana. Por defecto es None.
+        """
         super().__init__(parent, title='Cajero Automático')
         panel = wx.Panel(self)
 
@@ -23,15 +42,34 @@ class MenuPrincipal(wx.Frame):
         self.Show()
 
     def depositar_efectivo(self, event):
+        """
+        Abre la ventana para realizar un depósito de efectivo.
+        
+        Args:
+            event: El evento que activa la función (en este caso, el clic en el botón).
+        """
         from vistas.deposito_sinTarjeta_vista import DepositoSinTarjeta
         deposito = DepositoSinTarjeta(parent=self)
         deposito.Show()
 
     def pagar_servicios(self, event):
+        """
+        Abre la ventana para realizar el pago de servicios.
+        
+        Args:
+            event: El evento que activa la función (en este caso, el clic en el botón).
+        """
         from vistas.seleccionarServicio import SeleccionarServicio
         seleccionarServicio = SeleccionarServicio(self)
         seleccionarServicio.Show()
 
     def abrir_insertar_tarjeta(self, event):
+        """
+        Abre la ventana para insertar una tarjeta en el cajero.
+        
+        Args:
+            event: El evento que activa la función (en este caso, el clic en el botón).
+        """
         insertar_tarjeta_frame = InsertarTarjeta(self)
         insertar_tarjeta_frame.Show()
+
