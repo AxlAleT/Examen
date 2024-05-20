108cf3d207d1f691790e55b5f902ff9bcb338890
Omar AC <109171712+OmarAC03@users.noreply.github.com>
2024-05-20 09:01:50 -0600

    excepciones_tarjeta.py

--- Codigo/excepciones/excepciones_tarjeta.py
+++ Codigo/excepciones/excepciones_tarjeta.py
@@ -1,8 +1,17 @@
 class NumeroTarjetaIncorrecto(Exception):
+    """
+    Excepción lanzada cuando se proporciona un número de tarjeta incorrecto o inválido.
+    """
     pass
 
 class NipIncorrecto(Exception):
+    """
+    Excepción lanzada cuando se proporciona un NIP incorrecto para una tarjeta.
+    """
     pass
 
 class SaldoInsuficiente(Exception):
+    """
+    Excepción lanzada cuando el saldo de una tarjeta no es suficiente para realizar una operación.
+    """
     pass
