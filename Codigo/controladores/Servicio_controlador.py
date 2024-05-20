c4e55ef3113486fbd786640f67f26f96ff20a8ab
Omar AC <109171712+OmarAC03@users.noreply.github.com>
2024-05-20 08:30:32 -0600

    Servicio_controlador.py

--- Codigo/controladores/Servicio_controlador.py
+++ Codigo/controladores/Servicio_controlador.py
@@ -2,9 +2,21 @@ from bd.db_controlador import sesion
 from modelos.Servicio import Servicio
 
 class ServicioControlador:
+    """
+    Controlador para la gestión de los servicios.
+
+    Methods:
+        obtener_servicios(): Obtiene una lista de tuplas con el nombre del servicio y su número de convenio.
+    """
 
     @staticmethod
     def obtener_servicios():
+        """
+        Obtiene una lista de tuplas con el nombre del servicio y su número de convenio.
+
+        Returns:
+            list: Lista de tuplas (nombre del servicio, número de convenio).
+        """
         servicios = sesion.query(Servicio).all()
         lista_servicios = [(servicio.NombreServicio, servicio.Num_Convenio) for servicio in servicios]
         return lista_servicios
