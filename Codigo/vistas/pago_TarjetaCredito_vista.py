0dcbcc298c1b88e4444147249da32a457f364b7a
Omar AC <109171712+OmarAC03@users.noreply.github.com>
2024-05-20 10:48:00 -0600

    pago_TarjetaCredito_vista.py

--- Codigo/vistas/pago_TarjetaCredito_vista.py
+++ Codigo/vistas/pago_TarjetaCredito_vista.py
@@ -3,7 +3,27 @@ from controladores.pago_TarjetaCredito_controlador import PagoTarjetaCreditoCont
 from excepciones.excepciones_tarjeta import NumeroTarjetaIncorrecto
 
 class PagoTarjetaCredito(wx.Frame):
+    """
+    Clase que representa la ventana para realizar un pago a una tarjeta de crédito en un cajero automático.
+
+    Attributes:
+        parent (wx.Window): La ventana principal o padre de esta ventana.
+        numero_tarjeta (str): El número de tarjeta de crédito a la que se realizará el pago.
+
+    Methods:
+        __init__(parent, numero_tarjeta): Inicializa la ventana de pago a tarjeta de crédito.
+        pagar_tarjeta(event): Realiza el pago a la tarjeta de crédito.
+        volver_menu_principal(event): Cierra la ventana y vuelve al menú principal.
+    """
+
     def __init__(self, parent, numero_tarjeta):
+        """
+        Inicializa la ventana de pago a tarjeta de crédito.
+
+        Args:
+            parent (wx.Window): La ventana principal o padre de esta ventana.
+            numero_tarjeta (str): El número de tarjeta de crédito a la que se realizará el pago.
+        """
         super().__init__(parent, title='Pago a Tarjeta de Crédito', size=(300, 150))
         self.numero_tarjeta = numero_tarjeta
         panel = wx.Panel(self)
@@ -30,6 +50,12 @@ class PagoTarjetaCredito(wx.Frame):
         panel.SetSizer(vbox)
 
     def pagar_tarjeta(self, event):
+        """
+        Realiza el pago a la tarjeta de crédito.
+
+        Args:
+            event: El evento que activa la función (en este caso, el clic en el botón).
+        """
         monto_string = self.text_monto.GetValue()
 
         try:
@@ -44,6 +70,12 @@ class PagoTarjetaCredito(wx.Frame):
             wx.MessageBox(f'Error inesperado: {str(e)}', 'Error', wx.OK | wx.ICON_ERROR)
 
     def volver_menu_principal(self, event):
+        """
+        Cierra la ventana y vuelve al menú principal.
+
+        Args:
+            event: El evento que activa la función (en este caso, el clic en el botón).
+        """
         self.Close()
 
 # main function to run the application
